d4e5be91b85885b4c6878a9aeedc3d36a1679bc6
Sebastian Karlsson <ianertson@gmail.com>
2021-09-14 19:59:45 +0200

    use cmake

---
+++ build/CMakeFiles/list.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list.dir/src/list.c.o"
+  "CMakeFiles/list.dir/src/list.c.o.d"
+  "liblist.pdb"
+  "liblist.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
